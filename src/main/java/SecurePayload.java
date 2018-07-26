c102387861bb1eb0bf4a78bb2875e093200ab533
dandm <dandm@mail.ece.utoronto.ca>
2018-07-26 16:46:47 -0400

    Committing SecurePayload class
    Initial commit of the class and its unit test

---
+++ src/main/java/SecurePayload.java
@@ -0,0 +1,52 @@
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import java.io.Serializable;
+import java.security.*;
+
+public class SecurePayload implements Serializable {
+    private byte[] cipherData;
+    private byte[] encryptedSessionKey;
+    private byte[] iv;
+
+    public SecurePayload(byte[] plaintextData, PublicKey publicKey) throws GeneralSecurityException {
+        // generate a random session key
+        KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+        keyGen.init(128);
+        SecretKey sessionKey = keyGen.generateKey();  // generate random secret key
+
+        // use session key to encrypt payload. In the process, create and save initialization vector
+        Cipher aesCipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] ivBytes = new byte[aesCipher.getBlockSize()];
+        secureRandom.nextBytes(ivBytes);
+        iv = ivBytes.clone();
+        IvParameterSpec ivParameterSpec = new IvParameterSpec(ivBytes);
+        aesCipher.init(Cipher.ENCRYPT_MODE, sessionKey, ivParameterSpec);
+        cipherData = aesCipher.doFinal(plaintextData);
+
+        // wrap (encrypt) session key with user's public key and save
+        Cipher rsaCipher = Cipher.getInstance("RSA");
+        rsaCipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        encryptedSessionKey = rsaCipher.doFinal(sessionKey.getEncoded());
+    }
+
+    public byte[] getPayload(PrivateKey privateKey) throws GeneralSecurityException {
+        // unwrap the session key
+        Cipher rsaCipher = Cipher.getInstance("RSA");
+        rsaCipher.init(Cipher.UNWRAP_MODE, privateKey);
+        Key sessionKey = rsaCipher.unwrap(encryptedSessionKey, "AES", Cipher.SECRET_KEY);
+
+        // decrypt payload using session key and initialization vector
+        Cipher aesCipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+        IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
+        aesCipher.init(Cipher.DECRYPT_MODE, sessionKey, ivParameterSpec);
+        return aesCipher.doFinal(cipherData);
+    }
+
+    public byte[] getCipherData() {
+        return cipherData;
+    }
+
+}
---
+++ src/test/java/SecurePayloadTest.java
@@ -0,0 +1,157 @@
+import com.sun.xml.internal.messaging.saaj.util.ByteInputStream;
+import org.junit.Test;
+
+import javax.crypto.SecretKey;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.*;
+import java.util.Base64;
+
+import static org.junit.Assert.*;
+
+public class SecurePayloadTest {
+
+    @Test
+    public void testSmall() {
+        final String expectedPlainText = "hello world!";
+        final byte[] expectedBytes = expectedPlainText.getBytes(StandardCharsets.UTF_8);
+
+        KeyPairGenerator keyGen = null;
+        try {
+            keyGen = KeyPairGenerator.getInstance("RSA");
+        } catch (NoSuchAlgorithmException e) {
+            fail(e.getMessage());
+        }
+        keyGen.initialize(512);
+        KeyPair keyPair = keyGen.generateKeyPair();
+
+        SecurePayload securePayload = null;
+        try {
+            securePayload = new SecurePayload(expectedBytes, keyPair.getPublic());
+        } catch (GeneralSecurityException e) {
+            fail(e.getMessage());
+        }
+
+        byte[] plainTextBytes = null;
+        try {
+            plainTextBytes = securePayload.getPayload(keyPair.getPrivate());
+        } catch (GeneralSecurityException e) {
+            fail(e.getMessage());
+        }
+
+        assertArrayEquals(expectedBytes, plainTextBytes);
+
+        final String plainText = new String(plainTextBytes, StandardCharsets.UTF_8);
+        assertEquals(expectedPlainText, plainText);
+    }
+
+    @Test
+    public void testBigTextFile() {
+        String path = this.getClass().getClassLoader().getResource("bigtext.txt").getPath();
+        byte[] expectedBytes = null;
+        try {
+            expectedBytes = Files.readAllBytes(Paths.get(path));
+        } catch (IOException e) {
+            fail(e.getMessage());
+        }
+
+        KeyPairGenerator keyGen = null;
+        try {
+            keyGen = KeyPairGenerator.getInstance("RSA");
+        } catch (NoSuchAlgorithmException e) {
+            fail(e.getMessage());
+        }
+        keyGen.initialize(512);
+        KeyPair keyPair = keyGen.generateKeyPair();
+
+        SecurePayload securePayload = null;
+        try {
+            securePayload = new SecurePayload(expectedBytes, keyPair.getPublic());
+        } catch (GeneralSecurityException e) {
+            fail(e.getMessage());
+        }
+
+        byte[] plainTextBytes = null;
+        try {
+            plainTextBytes = securePayload.getPayload(keyPair.getPrivate());
+        } catch (GeneralSecurityException e) {
+            fail(e.getMessage());
+        }
+
+        assertArrayEquals(expectedBytes, plainTextBytes);
+
+        final String plainText = new String(plainTextBytes, StandardCharsets.UTF_8);
+        final String expectedPlainText = new String(expectedBytes, StandardCharsets.UTF_8);
+        assertEquals(expectedPlainText, plainText);
+    }
+
+    @Test
+    public void testSerializingAudioFile() {
+        String path = this.getClass().getClassLoader().getResource("quick_brown_fox.3gp").getPath();
+        byte[] expectedBytes = null;
+        try {
+            expectedBytes = Files.readAllBytes(Paths.get(path));
+        } catch (IOException e) {
+            fail(e.getMessage());
+        }
+
+        KeyPairGenerator keyGen = null;
+        try {
+            keyGen = KeyPairGenerator.getInstance("RSA");
+        } catch (NoSuchAlgorithmException e) {
+            fail(e.getMessage());
+        }
+        keyGen.initialize(512);
+        KeyPair keyPair = keyGen.generateKeyPair();
+
+        SecurePayload clientSidePayload = null;
+        try {
+            clientSidePayload = new SecurePayload(expectedBytes, keyPair.getPublic());
+        } catch (GeneralSecurityException e) {
+            fail(e.getMessage());
+        }
+
+        // write to stream to simulate sending file from client to server
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        ObjectOutputStream oos = null;
+        try {
+            oos = new ObjectOutputStream(buffer);
+        } catch (IOException e) {
+            fail(e.getMessage());
+        }
+        try {
+            oos.writeObject(clientSidePayload);
+        } catch (IOException e) {
+            fail(e.getMessage());
+        }
+
+        // read back out of stream to simulate receiving payload on server side
+        ByteArrayInputStream bis = new ByteArrayInputStream(buffer.toByteArray());
+        ObjectInputStream ois = null;
+        try {
+            ois = new ObjectInputStream(bis);
+        } catch (IOException e) {
+            fail(e.getMessage());
+        }
+
+        SecurePayload serverSidePayload = null;
+        try {
+            serverSidePayload = (SecurePayload) ois.readObject();
+        } catch (IOException e) {
+            fail(e.getMessage());
+        } catch (ClassNotFoundException e) {
+            fail(e.getMessage());
+        }
+
+        byte[] receivedBytes = null;
+        try {
+            receivedBytes = serverSidePayload.getPayload(keyPair.getPrivate());
+        } catch (GeneralSecurityException e) {
+            fail(e.getMessage());
+        }
+
+        assertArrayEquals(expectedBytes, receivedBytes);
+    }
+}
