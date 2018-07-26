ac5a7e75a8609e391d386e46c3cb411733ce490c
dandm <dandm@mail.ece.utoronto.ca>
2018-07-26 17:02:26 -0400

    Fixed project structure
    Created a package to store the SecurePayload class (this was missing).

--- src/test/java/SecurePayloadTest.java
+++ src/test/java/SecurePayloadTest.java
@@ -1,13 +1,11 @@
-import com.sun.xml.internal.messaging.saaj.util.ByteInputStream;
+import ca.utoronto.ece.cimsah.sp.SecurePayload;
 import org.junit.Test;
 
-import javax.crypto.SecretKey;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.security.*;
-import java.util.Base64;
 
 import static org.junit.Assert.*;
 
